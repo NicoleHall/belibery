0cba88bd6b95531d227cb40925130f4b6314f104
Nicole Hall <nicolealexandrahall@gmail.com>
2015-10-29 12:11:41 -0600

    did model testing

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,2 +1,18 @@
 class Fan < ActiveRecord::Base
+  validates :name, presence: true, format: { with: /\A[a-zA-Z ]+\z/, message: "can only containg letters" }
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: { in: 5..50 }
+  validate :no_tories
+  belongs_to :location
+
+  def no_tories
+    errors.add(:name, "Cannot be Torie CHANGE YOUR NAME") if name =="torie" || name == "Torie"
+  end
+
+  def nickname
+    "#{name}lieber"
+  end
+
+  
 end
--- test/models/fan_test.rb
+++ test/models/fan_test.rb
@@ -1,7 +1,90 @@
 require 'test_helper'
+require 'minitest/pride'
 
 class FanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def valid_attributes
+    {
+      name: "Ross Edfort",
+      email: "truebelieber@gmail.com",
+      location_id: 1
+    }
+  end
+
+  test "fan is created with valid attributes" do
+    fan = Fan.new(valid_attributes)  #we can test validity without having this going into the database
+
+    assert fan.valid?
+    assert_equal "Ross Edfort", fan.name
+    assert_equal "truebelieber@gmail.com", fan.email
+
+  end
+
+  test "fan is invalid when missing a name" do
+    fan = Fan.new({
+      email: "truebelieber@gmail.com",
+      location_id: 1
+    })
+
+    refute fan.valid?
+  end
+
+  test "fan is invalid when missing an email" do
+    fan = Fan.new({
+      name: "Ross Edfort",
+      location_id: 1
+    })
+
+    refute fan.valid?
+  end
+
+  test "fan is invalid with duplicate email" do
+    2.times {Fan.create(valid_attributes)}
+
+  result = Fan.where(email: "truebelieber@gmail.com")
+  assert_equal 1, result.count
+  end
+
+  test "fan name only contains only letters" do
+    fan = Fan.new(name: "Ro55", email: "truebelieber.gmail.com", location_id: 1)
+
+    refute fan.valid?
+  end
+
+  test "fan email must be between 5 to 50 characters" do
+
+    fan1 = Fan.create({
+      name: "Ross Edfort",
+      email: "true",
+      location_id: 1
+    })
+
+    fan2 = Fan.create({
+      name: "Ross buttfort",
+      email: "trueverylongstringherethatisoverfiftycharactersicant beleivethisissolong@gmail.com",
+      location_id: 1
+    })
+
+    refute fan1.valid?
+    refute fan2.valid?
+
+  end
+
+  test "fan cannot be named tori" do
+    tori = Fan.new(name: "Torie",
+    email: "truebelieber@gmail.com",
+    location_id: 1)
+
+    refute tori.valid?
+
+  end
+
+   test "fan has a belieber nickname" do
+    fan = Fan.new(valid_attributes)
+
+    assert_equal "Ross Edfortlieber", fan.nickname
+  end
+
+  should belong_to :location 
+
 end
